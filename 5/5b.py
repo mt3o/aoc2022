65b41b963bcefb4bd1f9cf76b1d3f7bdc5d5e13f
mt3o <teodor.kulej@gmail.com>
2022-12-05 09:21:20 +0100

    day5

---
+++ 5/5b.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+from parse import parse
+
+stacks_input = [l.replace("[", "").replace("]", "").replace("   ", "!") for l in """
+   [M]         [Z]   [V]   
+   [Z]   [P]   [L]   [Z][J]
+[S][D]   [W]   [W]   [H][Q]
+[P][V][N][D]   [P]   [C][V]
+[H][B][J][V][B][M]   [N][P]
+[V][F][L][Z][C][S][P][S][G]
+[F][J][M][G][R][R][H][R][L]
+[G][G][G][N][V][V][T][Q][F]
+""".split("\n") if l.strip() != ""]
+
+stacks = [deque() for _ in stacks_input[-1]]
+for line_nr, line in enumerate(stacks_input):
+    for stack_nr, stack_item in enumerate(line):
+        if stack_item != "!":
+            stacks[stack_nr].appendleft(stack_item)
+
+
+def parse_input(line):
+    format = "move {amount:d} from {source:d} to {destination:d}"
+    return parse(format, line).named
+
+
+def apply_command(command):
+    amount = command['amount']
+    source = command['source']
+    destination = command['destination']
+    temporary = [stacks[source - 1].pop() for _ in range(amount)]
+    temporary.reverse()
+    [stacks[destination - 1].append(item) for item in temporary]
+
+
+
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        data_raw = [line.strip() for line in f if line.startswith("move")]
+        data = [parse_input(line) for line in data_raw]
+        for command in data:
+            apply_command(command)
+
+        print("Top of each stack is:")
+        for n,stack in enumerate(stacks):
+            print(stack.pop(), end="")
